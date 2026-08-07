ecae575485bc5e5c90d8972ba51e2008982ed31b
memetreza34-ux <memetreza34@gmail.com>
2026-08-07 20:33:51 +0200

    fix: relax script CSP only for Vite React refresh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+function allowReactRefreshPreambleInDevelopment() {
+  return {
+    name: "db-peace-dev-csp",
+    apply: "serve",
+    enforce: "pre",
+    transformIndexHtml(html) {
+      return html.replace(
+        "script-src 'self';",
+        "script-src 'self' 'unsafe-inline';",
+      );
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [allowReactRefreshPreambleInDevelopment(), react()],
   server: {
     host: "127.0.0.1",
     port: 5173,
